1a33c56a53f6e5e2a23981802db62e52d2f90677
Glovania <9937412@schoolsnet.act.edu.au>
2024-05-13 21:20:49 +1000

    Pointer detected highlight mesh for PC snap.
    Test case worked. I will be modifying to make it fully worked as it should be.
    
    Created a custom global collision layer name for 'Pointer Interaction'.
    
    Added a specific script to handle the snap functionality.

---
+++ scripts/snap_in_pc_parts.gd
@@ -0,0 +1,9 @@
+extends XRToolsInteractableBody
+
+
+func _on_PanelBodySnapIn_pointer_entered():
+	print("Pointer entered the MeshInstance!")
+
+
+func _on_PanelBodySnapIn_pointer_exited():
+	print("Pointer exited the MeshInstance!")
