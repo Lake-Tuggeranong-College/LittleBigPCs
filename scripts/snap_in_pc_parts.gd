aaf89f5c288e457202725316b47fdfb620c33e33
Glovania <9937412@schoolsnet.act.edu.au>
2024-05-13 22:03:00 +1000

    Test case worked for all PC parts.
    One step closer to the actual implementation :3

--- scripts/snap_in_pc_parts.gd
+++ scripts/snap_in_pc_parts.gd
@@ -2,8 +2,64 @@ extends XRToolsInteractableBody
 
 
 func _on_PanelBodySnapIn_pointer_entered():
-	print("Pointer entered the MeshInstance!")
+	print("This is the snap for Panel.")
 
 
 func _on_PanelBodySnapIn_pointer_exited():
-	print("Pointer exited the MeshInstance!")
+	print("No snap indicated for Panel.")
+
+
+func _on_GPUBodySnapIn_pointer_entered():
+	print("This is the snap for GPU.")
+
+
+func _on_GPUBodySnapIn_pointer_exited():
+	print("No snap indicated for GPU.")
+
+
+func _on_MotherboardBodySnapIn_pointer_entered():
+	print("This is the snap for Motherboard.")
+
+
+func _on_MotherboardBodySnapIn_pointer_exited():
+	print("No snap indicated for Motherboard.")
+
+
+func _on_CPUBodySnapIn_pointer_entered():
+	print("This is the snap for CPU.")
+
+
+func _on_CPUBodySnapIn_pointer_exited():
+	print("No snap indicated for CPU.")
+
+
+func _on_FanBodySnapIn_pointer_entered():
+	print("This is the snap for Fan.")
+
+
+func _on_FanBodySnapIn_pointer_exited():
+	print("No snap indicated for Fan.")
+
+
+func _on_HHDBodySnapIn_pointer_entered():
+	print("This is the snap for HHD.")
+
+
+func _on_HHDBodySnapIn_pointer_exited():
+	print("No snap indicated for HHD.")
+
+
+func _on_PowerSupplyBodySnapIn_pointer_entered():
+	print("This is the snap for Power Supply.")
+
+
+func _on_PowerSupplyBodySnapIn_pointer_exited():
+	print("No snap indicated for Power Supply.")
+
+
+func _on_RAMBodySnapIn_pointer_entered():
+	print("This is the snap for RAM.")
+
+
+func _on_RAMBodySnapIn_pointer_exited():
+	print("No snap indicated for RAM.")
