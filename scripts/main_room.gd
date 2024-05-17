220ebbabdf4c6e5d2e20e819dab539ac14fe5d9f
Glovania <9937412@schoolsnet.act.edu.au>
2024-05-17 10:15:27 +1000

    Added door for 'Win' room.
    This is the final implementation for this project.

---
+++ scripts/main_room.gd
@@ -0,0 +1,33 @@
+extends Spatial
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_DoorTriggerArea_body_entered(body):
+	if body.name == "PlayerBody":
+		var door_trigger_animation = get_node("DoorOpenAnimation")
+		door_trigger_animation.play("open_door")
+		
+		var scene_change = get_node("SceneChangeDoor")
+		scene_change.visible = true
+#		print("Player has entered the door zone.")
+
+
+func _on_DoorTriggerArea_body_exited(body):
+	if body.name == "PlayerBody":
+		var door_trigger_animation = get_node("DoorOpenAnimation")
+		door_trigger_animation.play("close_door")
+		
+		var scene_change = get_node("SceneChangeDoor")
+		scene_change.visible = false
+#		print("Player has left the door zone.")
+
+
+func _on_SceneChangeDoor_body_entered(body):
+	if body.name == "PlayerBody":
+#		get_tree().change_scene("res://scenes/win/win_room.tscn")
+		print("Transfer player to the win room.")
+# TODO: Add conditon for winning (Condition: Player finished building the PC Tower).
